12afad7aec7a8a1dec10623dfe02e1cca93e652c
kleinborre <131511662+kleinborre@users.noreply.github.com>
2025-05-30 13:58:37 +0800

    Modified pom.xml and added list of dependencies required for this course.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package main;
 
-/**
- *
- * @author STUDY MODE
- */
 public class Main {
 
     public static void main(String[] args) {
