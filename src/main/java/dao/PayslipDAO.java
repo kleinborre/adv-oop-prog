95db7f66a23f10c0de0c771ab18c5be28f7aa42a
kleinborre <131511662+kleinborre@users.noreply.github.com>
2025-06-09 02:15:17 +0800

    Add code for PayslipDAO interface.

--- src/main/java/dao/PayslipDAO.java
+++ src/main/java/dao/PayslipDAO.java
@@ -1,5 +1,17 @@
 package dao;
 
+import pojo.Payslip;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface PayslipDAO {
-    
+
+    Payslip getPayslipByEmployeeAndPeriod(int employeeID, int payPeriodID) throws SQLException;
+
+    List<Payslip> getPayslipsByEmployeeID(int employeeID) throws SQLException;
+
+    List<Payslip> getAllPayslipsByPeriod(int payPeriodID) throws SQLException;
+
+    List<Payslip> getAllPayslips() throws SQLException;
 }
