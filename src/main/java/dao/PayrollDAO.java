96998be8411b02b4e5e7966d1d31fa295d0a5587
kleinborre <131511662+kleinborre@users.noreply.github.com>
2025-06-09 01:36:17 +0800

    Add code for PayrollDAO interface.

--- src/main/java/dao/PayrollDAO.java
+++ src/main/java/dao/PayrollDAO.java
@@ -1,5 +1,21 @@
 package dao;
 
+import pojo.Payroll;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface PayrollDAO {
-    
+
+    Payroll getPayrollByID(int payrollID) throws SQLException;
+
+    List<Payroll> getPayrollsByEmployeeID(int employeeID) throws SQLException;
+
+    List<Payroll> getAllPayrolls() throws SQLException;
+
+    void addPayroll(Payroll payroll) throws SQLException;
+
+    void updatePayroll(Payroll payroll) throws SQLException;
+
+    void deletePayroll(int payrollID) throws SQLException;
 }
